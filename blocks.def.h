aec5125a003cba3234f4a3d1d7b48de8477b336b
adrianFD22 <adrianfd22399@gmail.com>
2023-08-13 00:57:11 +0200

    Add some fields

--- blocks.def.h
+++ blocks.def.h
@@ -2,10 +2,13 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date +'%H:%M'",					5,		0},
+    {"", "dwmblocks-ip", 5, 0 },
+    {"", "dwmblocks-battery", 5, 0 },
+    {"", "dwmblocks-backlight", 5, 0},
+	{"", "dwmblocks-volume", 5, 0},
+	{"", "dwmblocks-clock", 60, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
