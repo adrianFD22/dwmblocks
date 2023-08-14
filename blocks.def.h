8a7590a61629e0b3b5a4a4e57cd74cf6380ed00f
adrianFD22 <adrianfd22399@gmail.com>
2023-08-14 17:54:25 +0200

    Fix signals

--- blocks.def.h
+++ blocks.def.h
@@ -1,14 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "dwmblocks-memory",	        30,		            0 },
-    {"", "dwmblocks-ip",                5,                  0 },
-    {"", "dwmblocks-battery",           5,                  0 },
-    {"", "dwmblocks-backlight",         5,                  0 },
-	{"", "dwmblocks-volume",            5,                  0 },
-	{"", "dwmblocks-clock",             60,                 0},
+    {"", "dwmblocks-ip",                5,                  1 },
+	{"", "dwmblocks-memory",	        10,		            2 },
+    {"", "dwmblocks-battery",           5,                  3 },
+    {"", "dwmblocks-backlight",         0,                  4 },
+	{"", "dwmblocks-volume",            0,                  5 },
+	{"", "dwmblocks-clock",             60,                 6 },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
