b4520643edc3827d5ca3a89249ddd373692c22fe
adrianFD22 <adrianfd22399@gmail.com>
2023-08-17 00:41:30 +0200

    Change config dir

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"", "dwmblocks-ip",                5,                  1 },
-	{"", "dwmblocks-memory",	        10,		            2 },
-    {"", "dwmblocks-battery",           10,                 3 },
-    {"", "dwmblocks-backlight",         0,                  4 },
-	{"", "dwmblocks-volume",            0,                  5 },
-	{"", "dwmblocks-clock",             60,                 6 },
+    {"", "$HOME/.config/dwmblocks/ip",                5,                  1 },
+	{"", "$HOME/.config/dwmblocks/memory",	        10,		            2 },
+    {"", "$HOME/.config/dwmblocks/battery",           10,                 3 },
+    {"", "$HOME/.config/dwmblocks/backlight",         0,                  4 },
+	{"", "$HOME/.config/dwmblocks/volume",            0,                  5 },
+	{"", "$HOME/.config/dwmblocks/clock",             60,                 6 },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
